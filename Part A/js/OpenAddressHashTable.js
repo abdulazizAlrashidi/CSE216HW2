3f7974b6fbccfda40c6fd465c9a440631627bf8b
Abdulaziz Alrashidi <aziz_alreshidy@hotmail.com>
2022-06-11 16:20:55 -0400

    third commit

--- Part A/js/OpenAddressHashTable.js
+++ Part A/js/OpenAddressHashTable.js
@@ -47,17 +47,104 @@ export default class OpenAddressHashTable {
     
     // @todo - YOU MUST DEFINE THIS METHOD
     getValue(key) {
-        let r  = 0;
+        let index = this.hashCode(key);
+        let count = 0;
+
+        while (count < this.length) {
+            let testKVP = this.hashTable[index];
+            if (testKVP == null) {
+                return null;
+            }
+            else if (testKVP.key == key) {
+                return testKVP.value;
+            }
+            index++;
+            if (index == this.length) {
+                index = 0;
+            }
+            count++;
+        }
         return null;
     }
     
     // @todo - YOU MUST DEFINE THIS METHOD
-    removeValue(key) {   
+    removeValue(key) {
+        let index = this.hashCode(key);
+        let count = 0;
+        while (count < this.length) {
+            let testKVP = this.hashTable[index];
+            if (testKVP == null) {
+                return;
+            } else if (testKVP.key == key) {
+                delete this.hashTable[index];
+                this.hashCode[index] = null;
+                this.size--;
+                let temp = [];
+                let counter = 0;
+                for (let i = 0; i < this.length; i++) {
+                    let item = this.hashTable[i];
+                    if (item != null) {
+                        temp[counter] = item;
+                        counter++;
+                    }
+                    this.hashTable[i] = null;
+                }
+                this.size = 0;
+                for (let i = 0; i < counter; i++) {
+                    let item = temp[i];
+                    let keyToRehash = item.key;
+                    let valueToRehash = item.value;
+                    this.putValue(keyToRehash, valueToRehash);
+
+                    delete temp[i];
+                }
+                return;
+            }
+            index++;
+            if (index == this.length) {
+                index = 0;
+            }
+            count++;
+        }
     }
 
     // @todo - YOU MUST DEFINE THIS METHOD
     putValue(key, item) {
-
+        let index = this.hashCode(key);
+        let count = 0;
+        while (count < this.length) {
+            let testKVP = this.hashTable[index];
+            if (testKVP == null) {
+                this.hashTable[index] = new KeyValuePair(key, item);
+                this.size++;
+                return;
+            } else if (testKVP.key == key) {
+                this.hashTable[index].value = item;
+                this.size++;
+                return;
+            }
+            index++;
+            if (index == this.length) {
+                index  = 0;
+            }
+            count++;
+        }
+        let temp = this.hashTable;
+        this.length = this.length * 2;
+        this.hashTable = [];
+        for (let i = 0; i < this.length; i++) {
+            this.hashTable[i] = null;
+        }
+        let numToCopy = this.size;
+        this.size = 0;
+        for (let i = 0; i < numToCopy; i++) {
+            let kvp = temp[i];
+            let keyToMove = kvp.key;
+            let valueToMove = kvp.value;
+            this.putValue(keyToMove, valueToMove);
+            delete temp[i];
+        }
+        this.putValue(key, item);
     }
     
     toString() {
